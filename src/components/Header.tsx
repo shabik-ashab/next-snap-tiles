4417a1cf2ccbd0bafddaa0e3fdbc592542eaf39b
shabik-ashab <shabik.ashab@gmail.com>
2024-06-30 21:49:06 +0600

    fix searchParams deployment

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { Suspense } from "react";
 import Search from "./Search";
+import Loading from "./Loading";
 
 const Header = () => {
   return (
@@ -7,7 +8,10 @@ const Header = () => {
       <div className="navbar bg-neutral shadow-lg">
         <div className="flex-1">
           <a className="btn btn-ghost text-xl">daisyUI</a>
-          <Search />
+          <Suspense fallback={<Loading />}>
+            <Search />
+          </Suspense>
+          
         </div>
         <div className="flex-none gap-2">
         </div>
